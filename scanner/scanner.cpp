acfdaf72d60c09accca6af04f6bf3ce06c8c4bd6
danielcaballero95 <48809928+danielcaballero95@users.noreply.github.com>
2019-03-24 02:11:51 -0700

    Add files via upload

--- scanner/scanner.cpp
+++ scanner/scanner.cpp
@@ -1,3 +1,4 @@
+
 #include<iostream>
 #include<fstream>
 #include<string>
@@ -9,58 +10,182 @@ using namespace std;
 
 // --------- DFAs ---------------------------------
 
+//Purpose: check to see if it's a vowel
+//Done by: Daniel Caballero
+bool vowelCheck(char c)
+{
+  switch(c){
+  case 'a':
+  case 'i':
+  case 'I':
+  case 'u':
+  case 'e':
+  case 'E':
+  case 'o':
+    //cout<<"vowel check"<<endl;
+    return true;
+    break;
+  default:
+    return false;
+  }
+}
+
+//Purpose: Check consonant Pairs
+//Done by Daniel Caballero
+bool checkConsPairs(string s, int &charpos, int& state)
+{
+  
+  char c = s[charpos];
+  //  cout<<s[charpos]<<" ss"<<endl;
+  switch (c) 
+    {
+    case 'b':
+    case 'm':
+    case 'k':
+    case 'n':
+    case 'h':
+    case 'p':
+    case 'r':
+    case 'y':
+    case 'g':
+      if (vowelCheck(s[charpos+1]))
+	{
+	  state=6;
+	  return true;
+	}
+      else if (s[charpos+1]== 'y')
+	{
+	  state=4;
+	  charpos++;
+	  return true;
+	}
+    
+      break;
+    case 's':
+      if (vowelCheck(s[charpos+1]))
+        {
+	  //	  cout<< endl << s[charpos+1];
+          state=6;
+          return true;
+        }
+      else if (s[charpos+1]== 'h')
+        {
+          state=4;
+          charpos++;
+          return true;
+        }
+      break;
+    case 't':
+      if (vowelCheck(s[charpos+1]))
+        {
+          state=6;
+          return true;
+        }
+      else if (s[charpos+1]== 's')
+        {
+          state=4;
+          charpos++;
+          return true;
+        }
+      break;
+    default:
+      return false;
+    }
+}
+
+
+
+
 // ** MYTOKEN DFA to be replaced by the WORD DFA
-// ** Done by:
+// ** Done by:Daniel Caballero
 // ** RE:
 bool word(string s)
 {
-	int state = 0;
-	int charpos = 0;
-
-	while (s[charpos] != '\0')
+  int state = 0;
+  int charpos = 0;
+  int tempState=0;
+  while (s[charpos] != '\0')
+    {
+      if ((state== 6))
+	state =0;    
+      else if (state == 0 && s[charpos] == 't')
+        state = 3;
+      else if (state == 0 && s[charpos] == 's')
+        state = 2;
+      else if (state == 0 && s[charpos] == 'c')
+        state = 1;
+      else if ((state == 0) && (vowelCheck(s[charpos])==true))
 	{
-		if (state == 0 && s[charpos] == 'a')
-			state = 1;
-		else
-		if (state == 1 && s[charpos] == 'b')
-			state = 2;
-		else
-		if (state == 2 && s[charpos] == 'b')
-			state = 2;
-		else
-			return(false);
-		charpos++;
-	}//end of while
+	  //	  cout<<"\t"<<vowelCheck(s[charpos])<<endl;
+	  state=6;
+	}
+      else if ((state == 0) && (checkConsPairs(s,charpos,tempState)==true))
+        state=tempState;
+      else if (state == 0 && s[charpos] == 'j')
+        state = 5;
+      else if(state == 0 && s[charpos] == 'z')
+        state = 5;
+      else if (state == 0 && s[charpos] == 'd')
+        state = 5;
+      else if (state == 0 && s[charpos] == 'w')
+        state = 5;
+      else if(state == 0 && s[charpos] == 'y')
+	state =5;
+      else if (state ==5 && vowelCheck(s[charpos]))
+	state = 6;
+      else if (state ==5 && s[charpos]=='s')
+	state =6;
+      else if (state ==5 && s[charpos]=='a')
+	state=6;
+      else if (state == 4 && vowelCheck(s[charpos]))
+	state =6;
+      else if (state == 4 && s[charpos]== 'y')
+	state =5;
+      else if (state == 3 && s[charpos] == 's')
+	state=5;
+      else if (state == 3 && vowelCheck(s[charpos]))
+	state=6;
+      else if (state == 2 && s[charpos] == 'h')
+	state=5;
+      else if (state == 2 && vowelCheck(s[charpos]))
+	state=6;
+      else if (state == 1 && s[charpos] == 'h')
+	state=5;
+      else 
+	return(false);
+      charpos++;
+      //cout<<state<<endl;
+    }//end of while
 
-	// where did I end up????
-	if (state == 2) return(true);  // end in a final state
-	else return(false);
+  // where did I end up????
+  if ((state == 6) || (state == 0)) return(true);  // end in a final state
+  else return(false);
 }
 
 // ** Add the PERIOD DFA here
 // ** Done by: Chantell Chapman
 bool periodDFA(string s)
 {
-	/*
-	if (s[0] == '.')
-		return true;
-	else
-		return false;
-	*/
+  /*
+    if (s[0] == '.')
+    return true;
+    else
+    return false;
+  */
 
-	int state = 0;
-	int charpos = 0;
+  int state = 0;
+  int charpos = 0;
 
-	while (s[charpos] != '\0') {
-		if (state == 0 && (s[charpos] == '.'))
-			state = 1;
-		else
-			return (false);
-		charpos++;
-	}
+  while (s[charpos] != '\0') {
+    if (state == 0 && (s[charpos] == '.'))
+      state = 1;
+    else
+      return (false);
+    charpos++;
+  }
 
-	if (state == 1) return (true);
-	else return (false);
+  if (state == 1) return (true);
+  else return (false);
 }
 
 
@@ -76,30 +201,31 @@ string tokenName[30] = { "WORD1", "WORD2", "PERIOD", "VERB", "VERBNEG", "VERBPAS
 // ** Do not require any file input for this.
 // ** a.out should work without any additional files.
 
+const int rIndexA=20;
+const int rIndexB=20;
+string reservedWords[rIndexA][rIndexB] =
+  {
+    {"masu", "VERB"},
+    { "masen", "VERBNEG"},
+    {"mashita", "VERBPAST"},
+    { "masendeshita", "VERBPASTNEG"},
+    {"desu", "IS"},
+    {  "deshita", "WAS"},
+    {"o", "OBJECT"},
+    {  "wa", "SUBJECT"},
+    {"ni", "DESTINATION"},
+    {  "watashi", "PRONOUN"},
+    {"anata", "PRONOUN"},
+    {"kare", "PRONOUN"},
+    {"kanojo", "PRONOUN"},
+    { "sore", "PRONOUN"},
+    { "mata", "CONNECTOR"},
+    {"soshite", "CONNECTOR"},
+    {"shikashi", "CONNECTOR"},
+    {"dakara", "CONNECTOR"},
+    {"eofm", "EOFM"}
 
-string reservedwords[20][2] =
-{
-  {	"masu", "VERB"},
-  { "masen", "VERBNEG"},
-  {	"mashita", "VERBPAST"},
-  { "masendeshita", "VERBPASTNEG"},
-  {	"desu", "IS"},
-  {  "deshita", "WAS"},
-  {	"o", "OBJECT"},
-  {  "wa", "SUBJECT"},
-  {	"ni", "DESTINATION"},
-  {  "watashi", "PRONOUN"},
-  {	"anata", "PRONOUN"},
-  {	"kare", "PRONOUN"},
-  {	"kanojo", "PRONOUN"},
-  { "sore", "PRONOUN"},
-  { "mata", "CONNECTOR"},
-  {"soshite", "CONNECTOR"},
-  {"shikashi", "CONNECTOR"},
-  {	"dakara", "CONNECTOR"},
-  {	"eofm", "EOFM"}
-
-};
+  };
 
 // ------------ Scaner and Driver ----------------------- 
 
@@ -112,11 +238,11 @@ int scanner(tokentype& a, string& w)
 {
 
   string currentWord; // To store the word for readability during processing
-  string endOfFileWord = reservedwords[18][0]; // Used for readability
+  string endOfFileWord = reservedWords[18][0]; // Used for readability
   bool isEndOfFile; 
 
-	// ** Grab the next word from the file via fin
-	// 1. If it is eofm, return right now.   
+  // ** Grab the next word from the file via fin
+  // 1. If it is eofm, return right now.   
   isEndOfFile = fin.eof();
 
   // Check if the file stream is empty
@@ -145,36 +271,71 @@ int scanner(tokentype& a, string& w)
   }
 
         
-	/*
-	2. Call the token functions one after another (if-then-else)
-	And generate a lexical error message if both DFAs failed.
-	Let the token_type be ERROR in that case.
+  /*
+    2. Call the token functions one after another (if-then-else)
+    And generate a lexical error message if both DFAs failed.
+    Let the token_type be ERROR in that case.
  
-	*/
+  */
 
   /*
-	4. Return the token type & string  (pass by reference)
-	*/
+    4. Return the token type & string  (pass by reference)
+  */
 
   // Check if its a valid word
   if( word( currentWord )  ) {
-
-  /*
-	3. Then, make sure WORDs are checked against the reservedwords list
-	If not reserved, token_type is WORD1 or WORD2.
-  */
-
+    
+    /*
+      3. Then, make sure WORDs are checked against the reservedwords list
+      If not reserved, token_type is WORD1 or WORD2.
+    */
+    
     // FOR DANIEL : (Note : The test labels all words as errors because there's no implementation here for the word check yet. It works for periods and end of files. )
-
+    //START Daniel Section
+    bool reservedFlag=false;
+    for (int i=0;i<rIndexA;i++){
+      if (currentWord==reservedWords[i][0]){
+	if (reservedWords[i][1]==tokenName[3])
+	  a=VERB;
+	else if (reservedWords[i][1]==tokenName[4])
+	  a=VERBNEG;
+	else if (reservedWords[i][1]==tokenName[5])
+	  a=VERBPAST;
+	else if (reservedWords[i][1]==tokenName[6])
+	  a=VERBPASTNEG;
+	else if (reservedWords[i][1]==tokenName[7])
+	  a=IS;
+	else if (reservedWords[i][1]==tokenName[8])
+	  a=WAS;
+	else if (reservedWords[i][1]==tokenName[9])
+	  a=OBJECT;
+	else if (reservedWords[i][1]==tokenName[10])
+	  a=SUBJECT;
+	else if (reservedWords[i][1]==tokenName[11])
+	  a=DESTINATION;
+	else if (reservedWords[i][1]==tokenName[12])
+	  a=PRONOUN;
+	else if (reservedWords[i][1]==tokenName[13])
+	  a=CONNECTOR;
+	reservedFlag=true;
+	  }
     }
+    if (((currentWord[currentWord.length()-1] <=90)) && ((currentWord[currentWord.length()-1]>=65)&& reservedFlag==false))//uppercase
+	a=WORD2;
+    else if (reservedFlag==false) 
+	a=WORD1;	
+	  
+    
 
-    // Check if its a period
-    else if ( periodDFA( currentWord ) ) {
+  }//end DANIEL SECTION
 
-      a = PERIOD;
+  // Check if its a period
+  else if ( periodDFA( currentWord ) ) {
+
+    a = PERIOD;
   }
 
-    // If the word wasn't a valid token, show a lexical error
+  // If the word wasn't a valid token, show a lexical error
   else {
 
     a = ERROR;
@@ -194,26 +355,26 @@ int scanner(tokentype& a, string& w)
 // Done by:  Rika
 int main()
 {
-	tokentype thetype;
-	string theword;
-	string filename;
+  tokentype thetype;
+  string theword;
+  string filename;
 
-	cout << "Enter the input file name: ";
-	cin >> filename;
+  cout << "Enter the input file name: ";
+  cin >> filename;
 
-	fin.open(filename.c_str());
+  fin.open(filename.c_str());
 
-	// the loop continues until eofm is returned.
-	while (true)
-	{
-		scanner(thetype, theword);  // call the scanner
-		if (theword == "eofm") break;  // stop now
+  // the loop continues until eofm is returned.
+  while (true)
+    {
+      scanner(thetype, theword);  // call the scanner
+      if (theword == "eofm") break;  // stop now
 
-		cout << "Type is:" << tokenName[thetype] << endl;
-		cout << "Word is:" << theword << endl;
-	}
+      cout << "Type is:" << tokenName[thetype] << endl;
+      cout << "Word is:" << theword << endl<<endl;
+    }
 
-	cout << "End of file is encountered." << endl;
-	fin.close();
+  cout << "End of file is encountered." << endl;
+  fin.close();
 
 }// end
